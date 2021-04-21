40f34d28947adc6dd0a342df9be41c2264371898
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2021-04-21 15:00:18 +0545

    Update size_config.dart

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -51,3 +51,18 @@ class SizeConfig {
 //     safeBlockVertical = (screenHeight - _safeAreaVertical) / 100;
 //   }
 // }
+
+// OR 
+
+// import 'package:flutter/material.dart';
+
+// double getProportionateScreenHeight(double inputHeight, BuildContext context) {
+//   double screenHeight = MediaQuery.of(context).size.height;
+//   return (inputHeight / 812.0) * screenHeight;
+// }
+
+// double getProportionateScreenWidth(double inputWidth, BuildContext context) {
+//   double screenWidth = MediaQuery.of(context).size.width;
+//   return (inputWidth / 375.0) * screenWidth;
+// }
+
